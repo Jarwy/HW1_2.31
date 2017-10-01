faa32a2c88b84eeb51b9c4c5f9f2b5b80e5b2b1a
Jarwy <jarwy880413@gmail.com>
2017-10-01 21:13:29 +0800

    first commit

---
+++ source/main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+int main(void)
+{
+	int n, u, m;
+
+	printf("number  square  cube\n");
+	printf("%d\t%d\t%d\t\n",0,0*0,0*0*0);
+	printf("%d\t%d\t%d\t\n", 1, 1 * 1, 1 * 1 * 1);
+	printf("%d\t%d\t%d\t\n", 2, 2 * 2, 2 * 2 * 2);
+	printf("%d\t%d\t%d\t\n", 3, 3 * 3, 3 * 3* 3);
+	printf("%d\t%d\t%d\t\n", 4, 4 * 4, 4 * 4 * 4);
+	printf("%d\t%d\t%d\t\n", 5, 5 * 5, 5 * 5 * 5);
+	printf("%d\t%d\t%d\t\n", 6, 6 * 6, 6 * 6 * 6);
+	printf("%d\t%d\t%d\t\n", 7, 7 * 7, 7 * 7 * 7);
+	printf("%d\t%d\t%d\t\n", 8, 8 * 8, 8 * 8 * 8);
+	printf("%d\t%d\t%d\t\n", 9, 9 * 9, 9 * 9 * 9);
+	printf("%d\t%d\t%d\t\n", 10, 10 * 10, 10 * 10 * 10);
+
+	
+
+	
+	system("pause");
+	return 0;
+}
+
